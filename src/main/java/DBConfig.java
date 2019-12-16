3bb738961008fcf8557dfb2494bb90976831159b
joshkallagunta <nq6647xv@go.minneapolis.edu>
2019-12-16 03:48:46 -0600

    Update DB Url

--- src/main/java/DBConfig.java
+++ src/main/java/DBConfig.java
@@ -1,8 +1,9 @@
 public class DBConfig {
 
-    static String db_url = "jdbc:sqlite:/Users/joshkallagunta/IdeaProjects/Workout/Workouts.db";
+    static String db_url = "jdbc:sqlite:Workout.sqlite";
 
     //change this, jdbc:sqlite:Workout.sqlite
 
+    //jdbc:sqlite:/Users/joshkallagunta/IdeaProjects/Workout/Workouts.db
 
 }
